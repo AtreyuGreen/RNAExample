edd7e05f780186370888cb90a17913574e48bac9
AtreyuGreen <g_atreyu@hotmail.com>
2019-06-18 23:18:17 +0200

    Mejora del ejemplo 1, y recoger datos desde un csv.

--- data/Example1DataAgent.py
+++ data/Example1DataAgent.py
@@ -1,3 +1,4 @@
+import os  
 import random
 import numpy as np
 import pandas as pd
@@ -14,28 +15,18 @@ class Example1DataAgent(IDataAgent):
     La columna Result es el resultado final.
     '''
     def retrieveDataFromSource(self):
-        #Este metodo calculara un simple algoritmo con resultado que sea par o impar, algo bastante simple.
-        columnA = []
-        columnB = []
-        columnC = []
-        result = []
+        self.csvPath = "test1.csv"
+        if(not os.path.isfile(self.csvPath)):
+            print "############ CREANDO DATOS..............."
+            self.data = pd.read_csv(self.csvPath) 
+        else:
+            print "&&&&&&&&&&&&& CARGANDO DATOS................."
+            self._createCsv()
 
-        for index  in range(1, 2000):
-            a_object = random.randint(1, 10)
-            b_object = random.randint(1, 26)
-            c_object = random.randint(1, 75)
-            r_object = (int(a_object + (2*b_object) -c_object)) % 2
-            columnA.append(a_object)
-            columnB.append(b_object)
-            columnC.append(c_object)
-            result.append(r_object)
-        
-        d = {'x':columnA, 'y':columnB, 'z':columnC, 'resultado':result}
-        self.data = pd.DataFrame(data = d)
 
     def createTrainingDataSet(self, test_size):
         # Specify the data 
-        X=self.data.ix[:,1:4]
+        X=self.data.ix[:,0:3]
         # Specify the target labels and flatten the array
         Y= np.ravel(self.data.resultado)
         # Split the data up in train and test sets
@@ -49,3 +40,30 @@ class Example1DataAgent(IDataAgent):
         self.validationDataSet.setTrainSet(X_test)
         self.validationDataSet.setResultSet(y_test)
         self.validationDataSet.scaleData()
+
+    def _createCsv(self):
+        #Este metodo calculara un simple algoritmo con resultado que sea par o impar, algo bastante simple.
+        columnA = []
+        columnB = []
+        columnC = []
+        result = []
+
+        for index  in range(1, 2000):
+            a_object = random.randint(1, 10)
+            b_object = random.randint(1, 26)
+            c_object = random.randint(1, 75)
+            #Analizamos si todos los valores estan debajo del mayor medio.
+            #Si es asi consideramos que la etiqueta es por debajo de la media.
+            if a_object < 5 and b_object < 13 and c_object < 37:
+                r_object = 1
+            else:
+                r_object = 0
+
+            columnA.append(a_object)
+            columnB.append(b_object)
+            columnC.append(c_object)
+            result.append(r_object)
+        
+        d = {'C1':columnA, 'C2':columnB, 'C3':columnC, 'resultado':result}
+        self.data = pd.DataFrame(data = d)
+        self.data.to_csv (self.csvPath, index = None, header=True) #Don't forget to add '.csv' at the end of the path        
--- example1.py
+++ example1.py
@@ -1,20 +1,54 @@
+import datetime
 from core.NeuronalNetwork import NeuronalNetwork
 from data.Example1DataAgent import Example1DataAgent
 from core.NeuronalNetworkLayer import NeuronalNetworkLayer
 
-data = Example1DataAgent()
-data.retrieveDataFromSource()
-data.createTrainingDataSet(0.33)
 
-model = NeuronalNetwork()
-layers = [
-    NeuronalNetworkLayer(3, 'relu'),
-    NeuronalNetworkLayer(8, 'relu'),
-    NeuronalNetworkLayer(1, 'sigmoid')
-]
+def run(loss, optimizer):
+    start_time = datetime.datetime.now()
+    data = Example1DataAgent()
+    data.retrieveDataFromSource()
+    data.createTrainingDataSet(0.33)
 
-model.setLayer(layers)
-model.create()
-model.train(data.trainDataSet)
-print(model.evaluate_model(data.validationDataSet))
-#print(model.predict(data.validationDataSet))
+    model = NeuronalNetwork()
+    layers = [
+        NeuronalNetworkLayer(3, 'relu'),
+        NeuronalNetworkLayer(8, 'relu'),
+        NeuronalNetworkLayer(1, 'sigmoid')
+    ]
+
+    model.setLayer(layers)
+    model.create()
+    model.train(data.trainDataSet, loss, optimizer)
+    print(model.evaluate_model(data.validationDataSet))
+    end_time = datetime.datetime.now()
+    print "Tiempo de entrenamiento: "+str((end_time - start_time).seconds)+" segundos"
+    #print(model.predict(data.validationDataSet))
+
+def test_1_run():
+    run('binary_crossentropy', 'sgd')
+
+def test_2_run():
+    run('mean_squared_error', 'sgd')
+
+def test_3_run():
+    run('binary_crossentropy', 'adam')
+
+def test_4_run():
+    run('mean_squared_error', 'adam')
+
+#El primer test, se realizara con 8 neuronas hidden, con una funcion de error de 
+#binary_crossentroypy y un optimizador de sgd
+#test_1_run()
+
+#El segundo test, se realizara con 8 neuronas hidden, con una funcion de error de 
+#mean_squared_error y un optimizador de sgd
+#test_2_run()
+
+#El tercer test, se realizara con 8 neuronas hidden, con una funcion de error de 
+#binary_crossentropy y un optimizador de adam
+#test_3_run()
+
+#El cuarto test, se realizara con 8 neuronas hidden, con una funcion de error de 
+#mean_squared_error y un optimizador de adam
+test_4_run()
